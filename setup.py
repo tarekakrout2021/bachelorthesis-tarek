7fa044863b4f910b0668b8322ad780315ca987cb
titi <takrout8@gmail.com>
2024-04-08 07:21:15 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup  # type: ignore
+
+setup(
+    name="bachelorthesis",
+    version="0.1.0",
+    description="bachelorthesis",
+    author="Tarek Akrout",
+    # license='MIT',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+    ],
+    # install_requires=["numpy", "tqdm"],
+    python_requires=">=3.10.12",
+)
